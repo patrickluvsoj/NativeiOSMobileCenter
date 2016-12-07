f80296e2fc98d074a257740416187d8e8edabaab
patrickluvsoj <pasuzuk@microsoft.com>
2016-12-07 10:59:44 -0800

    Initial Commit

---
+++ NativeiOSMobileCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NativeiOSMobileCenter
+//
+//  Created by Xamarin Demo on 12/7/16.
+//  Copyright © 2016 Xamarin Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NativeiOSMobileCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NativeiOSMobileCenter
+//
+//  Created by Xamarin Demo on 12/7/16.
+//  Copyright © 2016 Xamarin Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
